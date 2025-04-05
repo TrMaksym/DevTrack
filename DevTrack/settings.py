d1b6cd71f9b0d614ad318a6bf3e76c71f03e8a0d
unknown <maximuschampion2002@gmail.com>
2025-04-05 15:20:10 +0300

    Update Secret_key

--- DevTrack/settings.py
+++ DevTrack/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from TaskTracker import context_processors
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@d9qsm#bxauwnb9^rexla3(mt$=^5lz@8*z+pm^ww1-h3*h0^&'
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
